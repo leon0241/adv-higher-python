3de960c358922e26cce72b1d336673022389db32
Leon <enliang.leon.lee@gmail.com>
2020-09-03 12:50:14 +0100

    idk

--- Term 1/maze_task.py
+++ Term 1/maze_task.py
@@ -1,11 +1,11 @@
 maze = [[0 for x in range(7)] for y in range(7)]
+wallName = "H"
+doorName = "d"
 
 def setup_maze():
     walls = [2, 4, 6]
     floors = [1, 3, 5]
     mazeSize = 7
-    wallName = "□"
-    doorName = "d"
 
     #Walls (x)
     for x in range(0, mazeSize):
@@ -37,7 +37,6 @@ def setup_maze():
         doorx = doorArr[i][0]
         doory = doorArr[i][1]
         maze[doorx][doory] = doorName
-
     return floorArr, doorArr
 
 def print_maze():
@@ -50,11 +49,53 @@ def print_maze():
 
 def navigate_maze(ax, ay):
     roomNumber = 0
-    for i in range(0, 3):
-        coordX = ax[roomNumber][0]
-        coordY = ax[roomNumber][1]
-        print(coordX)
-        print(coordY)
+    mazeRunning = True
+    wallCheck = [False] * 4
+    while mazeRunning == True:
+        coordX = ax[roomNumber][1]
+        coordY = ax[roomNumber][0]
+
+        print("coords: " + str(coordX) + str(coordY))
+
+        directions = [ #room = 1 - 1
+            [coordX, (coordY - 1)], #this is north 1 - 0
+            [(coordX + 1), coordY], #this is east 2 - 1
+            [coordX, (coordY + 1)], #this is south 1 - 2
+            [(coordX - 1), coordY]  #this is west 0 - 1
+        ]
+
+        print("X" + str(directions[0][0]) + "Y" + str(directions[0][1]))
+        print("X" + str(directions[1][0]) + "Y" + str(directions[1][1]))
+        print("X" + str(directions[2][0]) + "Y" + str(directions[2][1]))
+        print("X" + str(directions[3][0]) + "Y" + str(directions[3][1]))
+
+        #for i in range(0, 3):
+        if maze[directions[0][0]][directions[0][1]] == wallName:
+            wallCheck[0] = False
+        else:
+            wallCheck[0] = True
+        print("X:" + str(directions[0][0]) + "Y:" + str(directions[0][1]) + maze[directions[0][0]][directions[0][1]] )
+
+        if maze[directions[1][0]][directions[1][1]] == wallName:
+            wallCheck[1] = False
+        else:
+            wallCheck[1] = True
+        print("X:" + str(directions[1][0]) + "Y:" + str(directions[1][1]) + maze[directions[1][0]][directions[1][1]])
+
+        if maze[directions[2][0]][directions[2][1]] == wallName:
+            wallCheck[2] = False
+        else:
+            wallCheck[2] = True
+        print("X:" + str(directions[2][0]) + "Y:" + str(directions[2][1]) + maze[directions[2][0]][directions[2][1]])
+
+        if maze[directions[3][0]][directions[3][1]] == wallName:
+            wallCheck[3] = False
+        else:
+            wallCheck[3] = True
+        print("X:" + str(directions[3][0]) + "Y:" + str(directions[3][1]) + maze[directions[3][0]][directions[3][1]])
+
+        print(wallCheck)
+
         roomNumber += 1
 
 floorArr, doorArr = setup_maze()
