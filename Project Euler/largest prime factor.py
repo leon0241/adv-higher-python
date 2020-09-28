e0fdb4a76b63bbe12c6673ee30844ed37f58e9de
Leon024 <enliang.leon.lee@gmail.com>
2020-09-28 15:21:45 +0100

    slow af

--- Project Euler/largest prime factor.py
+++ Project Euler/largest prime factor.py
@@ -1,20 +1,29 @@
 #The prime factors of 13195 are 5, 7, 13 and 29.
 #What is the largest prime factor of the number 600851475143 ?
 
-value = 13195
+value = 600851475143
 primeList = []
 primeFactors = []
 
-for i in range(1, value):
-    if value % i == 0:
-        primeList.append(i)
-for j in primeList:
-    factors = 0
-    for k in range(1, primeList[j]):
-        if j % k == 0:
-            factors += 1
-    if factors <= 2:
-        primeFactors.append(primeList[j])
+def find_factors(value, list):
+    for i in range(1, value):
+        if value % i == 0:
+            list.append(i)
+    return list
 
+def find_prime_factors(value, list):
+    primeFactorsList = []
+    for i in range(len(list)):
+        factors = 0
+        for j in range(1, list[i]):
+            if list[i] % j == 0:
+                factors += 1
+        if factors <= 2:
+            primeFactorsList.append(list[i])
+    return primeFactorsList
+
+
+primeList = find_factors(value, primeList)
+primeFactors = find_prime_factors(value, primeList)
 print(primeList)
 print(primeFactors)
